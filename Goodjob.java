6db327d597cc8b2664ac91abff0a07724b1daf30
mrhanliang <853449289@qq.com>
2015-11-04 22:22:27 +0800

    Change print

--- Goodjob.java
+++ Goodjob.java
@@ -1,6 +1,6 @@
 class Goodjob
 {
 	public static void main(String args[]){
-	System.out.println("刚才那球真是漂亮！！");
+	System.out.println("Goodjob!");
 	}
 }
--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
 	public static void main(String args[]){
 		System.out.println("Hello,world!");
-		System.out.println("知之为知之，不知为不知，是知也！是的");
+		System.out.println("知之为知之不知为不知是知也");
 	}
 }
