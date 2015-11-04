8c34f09c466818c5dd73242a061ed15de217e8f3
mrhanliang <853449289@qq.com>
2015-11-04 16:42:00 +0800

    Change print

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
 	public static void main(String args[]){
 		System.out.println("Hello,world!");
-		System.out.println("知之为知之");
+		System.out.println("知之为知之，不知为不知，是知也！");
 	}
 }
