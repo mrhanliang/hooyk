106f17b6385e46edbdfe1c7295ea9e7bfa5705c3
mrhanliang <853449289@qq.com>
2015-11-11 12:15:41 +0800

    第5次-Level3-韩亮-2015210862

---
+++ 第5次-Level3-韩亮-2015210862/BagTest.java
@@ -0,0 +1,19 @@
+public class BagTest
+{
+	public static void main(String args[]){
+		Bag bag = new Bag();
+
+		bag.insert("Monday");
+		bag.insert("Tuseday");
+		bag.insert("Wednesday");
+
+		System.out.println("集合中元素为：" + bag.toString());
+
+		bag.remove("Tuseday");
+
+		System.out.println("删除Tuesday后，集合中元素为：" + bag.toString());
+		System.out.println("查找Monday的结果为：" + bag.find("Monday"));
+		System.out.println("判断集合是否为空的结果为：" + bag.isEmpty());
+		bag.insert("Monday");
+	}
+}
