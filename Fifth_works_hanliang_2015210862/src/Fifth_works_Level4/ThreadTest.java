3c0341471d047101b25a55983ca93724e2313b21
mrhanliang <853449289@qq.com>
2015-11-19 15:17:47 +0800

    Handing over

---
+++ Fifth_works_hanliang_2015210862/src/Fifth_works_Level4/ThreadTest.java
@@ -0,0 +1,12 @@
+package Fifth_works_Level4;
+
+public class ThreadTest {
+	public static void main(String[] args) {
+		Fork fork = new Fork();
+		new Philosopher("0", fork).start();
+		new Philosopher("1", fork).start();
+		new Philosopher("2", fork).start();
+		new Philosopher("3", fork).start();
+		new Philosopher("4", fork).start();
+	}
+}
